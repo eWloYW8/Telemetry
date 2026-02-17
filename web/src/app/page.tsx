0ce71a166ea11760f07557446e25d4c2a05c3d7c
eWloYW8 <3171132517@qq.com>
2026-02-17 22:58:08 +0800

    refactor: move dashboard components

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardShell } from "@/features/dashboard/page-shell";
+import { DashboardShell } from "@/components/dashboard/page-shell";
 
 export default function Page() {
   return <DashboardShell />;
